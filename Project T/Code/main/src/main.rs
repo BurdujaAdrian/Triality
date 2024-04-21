cd248d6ae770f414fd2a981907b8db22b5245e73
BurdujaAdrian <adrianburduja7.3@gmail.com>
2024-04-21 18:57:27 +0300

    some struct

--- Project T/Code/main/src/main.rs
+++ Project T/Code/main/src/main.rs
@@ -1,3 +1,30 @@
+//use bevy::prelude::*;
+
+struct field{
+    
+}
+
+struct card_zone{
+
+}
+
+struct frame_data{
+
+}
+
+struct effect{
+
+}
+struct card{
+    damage:u32,
+    f_data:frame_data,
+    card_effect:effect
+}
+
+struct card_instance{
+
+}
+
 fn main() {
     println!("Hello, world!");
 }
