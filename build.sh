245f3cac75c3643b95296ab4338680849b82fe0c
BurdujaAdrian <adrianburduja7.3@gmail.com>
2024-06-17 14:16:28 +0300

    4. file IO

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 libs=-luser32
-warnings="-Wno-writable-strings -Wno-format-security"
+warnings="-Wno-writable-strings -Wno-format-security -Wno-deprecated-declarations"
 clang++ -g src/main.cpp -otri.exe $libs $warnings
